33a4f53ca3a3dd302302876ef6b466efce64c67b
Yossi Mosbacher <joe.mosbacher>
2021-03-30 15:53:54 +0300

    Bump version: 0.4.21 → 0.4.22

--- xepmts/__init__.py
+++ xepmts/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Yossi Mosbacher"""
 __email__ = 'joe.mosbacher@gmail.com'
-__version__ = '0.4.21'
+__version__ = '0.4.22'
 
 from xepmts.db.client import default_client, get_client
 from . import streams
