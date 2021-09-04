21a6ccf0ef4de4b3f076a0145844ffe4229dd708
Yossi Mosbacher <joe.mosbacher>
2021-09-04 12:33:09 +0300

    back-support and update deps

--- xepmts/__init__.py
+++ xepmts/__init__.py
@@ -5,11 +5,12 @@ __email__ = 'joe.mosbacher@gmail.com'
 __version__ = '0.5.5'
 
 import xeauth
-from xepmts.db.client import default_client, get_client, get_admin_client
+from xepmts.db.client import default_client, get_admin_client
+from xepmts.db.client import get_client as _get_client
 from . import streams
 from .settings import config
 
-def login(version,
+def login(version='v2',
           username=None,
           password=None,
           token=None, 
@@ -26,7 +27,14 @@ def login(version,
         xetoken = xeauth.user_login(username=username, password=password, scope=scope, audience=audience, **auth_kwargs )
     else:
         xetoken = xeauth.login(scopes=scope, audience=audience, **auth_kwargs)
-    return get_client(version, xetoken=xetoken, **kwargs)
+    try:
+        if xetoken.expired:
+            xetoken.refresh_tokens()
+    except:
+        pass
+    return _get_client(version, xetoken=xetoken, **kwargs)
+
+get_client = login
 
 def settings(**kwargs):
     from eve_panel import settings as panel_settings
