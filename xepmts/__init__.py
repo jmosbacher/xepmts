d4bb23a4000e481f2ce3c194c945656ae629377f
Yossi Mosbacher <joe.mosbacher>
2021-09-04 12:15:21 +0300

    Bump version: 0.5.4 → 0.5.5

--- xepmts/__init__.py
+++ xepmts/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Yossi Mosbacher"""
 __email__ = 'joe.mosbacher@gmail.com'
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 
 import xeauth
 from xepmts.db.client import default_client, get_client, get_admin_client
